dbb9f6bb64747dfea43bb7fab957173455544eb4
isaac844966 <isaackeyz66@gmai.com>
2024-05-13 12:37:42 +0200

    second commit

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const submitForm = function (event) {
   event.preventDefault();
   window.location.href = "dashboard.html";
 };
+
+const passwordInput = document.getElementById("password");
+const togglePass = document.getElementById("togglePass");
+
+const togglePassVisibility = function () {
+  passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+  togglePass.textContent = passwordInput.type === "password" ? "SHOW" : "HIDE";
+};
+
+togglePass.addEventListener("click", togglePassVisibility);
